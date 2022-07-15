d2705693ada53ec0669abfa39f6da0ae6d196bad
Julien Plée <julplee@gmail.com>
2022-07-15 16:01:17 +0200

    feat(json-files): now read several files

--- main.go
+++ main.go
@@ -86,92 +86,112 @@ func main() {
 
 	var mergeRequestsData JsonMergeRequestsData
 
-	// Open our jsonMergeRequests
-	jsonMergeRequests, err := os.Open("../gitlab-data.json")
-	// if we os.Open returns an error then handle it
+	f, err := os.Open("./data")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println("Successfully Opened JSON file")
 
-	byteValue, _ := ioutil.ReadAll(jsonMergeRequests)
-	json.Unmarshal(byteValue, &mergeRequestsData)
+	files, err := f.Readdir(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	layoutAt := "2006-01-02T15:04:05Z"
-	layoutDate := "2006-01-02T15:04:05Z07:00"
+	for _, v := range files {
+		if v.IsDir() {
+			continue
+		}
 
-	for _, mergeRequest := range mergeRequestsData.Data.Project.MergeRequests.Nodes {
-
-		timeCreatedAt, err := time.Parse(layoutAt, mergeRequest.CreatedAt)
+		// Open our jsonMergeRequests
+		jsonMergeRequests, err := os.Open("./data/" + v.Name())
+		// if we os.Open returns an error then handle it
 		if err != nil {
 			fmt.Println(err)
 		}
+		fmt.Println("Successfully Opened JSON file")
 
-		timeMergedAt, err := time.Parse(layoutAt, mergeRequest.MergedAt)
-		if err != nil {
-			fmt.Println(err)
-		}
+		byteValue, _ := ioutil.ReadAll(jsonMergeRequests)
+		json.Unmarshal(byteValue, &mergeRequestsData)
 
-		firstActivityDate := timeCreatedAt
-		for k := 0; k < len(mergeRequest.Commits.Nodes); k++ {
-			commitDate, err := time.Parse(layoutDate, mergeRequest.Commits.Nodes[k].AuthoredDate)
+		layoutAt := "2006-01-02T15:04:05Z"
+		layoutDate := "2006-01-02T15:04:05Z07:00"
+
+		for _, mergeRequest := range mergeRequestsData.Data.Project.MergeRequests.Nodes {
+
+			timeCreatedAt, err := time.Parse(layoutAt, mergeRequest.CreatedAt)
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			if firstActivityDate.After(commitDate) {
-				firstActivityDate = commitDate
+			timeMergedAt, err := time.Parse(layoutAt, mergeRequest.MergedAt)
+			if err != nil {
+				fmt.Println(err)
 			}
 
-			if firstActivityDate.After(timeMergedAt) {
-				fmt.Println("ERROR commit date is after merge date" + mergeRequest.Url)
-				os.Exit(1)
+			firstActivityDate := timeCreatedAt
+			for k := 0; k < len(mergeRequest.Commits.Nodes); k++ {
+				commitDate, err := time.Parse(layoutDate, mergeRequest.Commits.Nodes[k].AuthoredDate)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				if firstActivityDate.After(commitDate) {
+					firstActivityDate = commitDate
+				}
+
+				if firstActivityDate.After(timeMergedAt) {
+					fmt.Println("ERROR commit date is after merge date" + mergeRequest.Url)
+					os.Exit(1)
+				}
+			}
+
+			_, week := timeMergedAt.ISOWeek()
+
+			cycleTime := timeMergedAt.Sub(firstActivityDate).Hours()
+			totalCycleTimeDay := cycleTime / 24
+
+			reviewTime := timeMergedAt.Sub(timeCreatedAt).Hours()
+			reviewTimeDay := reviewTime / 24
+
+			commitToMergeRequestionCreationTime := timeCreatedAt.Sub(firstActivityDate).Hours()
+			commitToMergeRequestionCreationTimeDay := commitToMergeRequestionCreationTime / 24
+
+			team := membersTeam[mergeRequest.Author.Name]
+
+			if _, ok := analyzedMergeRequestsByTeam[team]; ok {
+				var analyzedMergeRequest AnalyzedMergeRequest
+				analyzedMergeRequest.Id = mergeRequest.Id
+				analyzedMergeRequest.Author = mergeRequest.Author.Name
+				analyzedMergeRequest.UserDiscussionsCount = mergeRequest.UserDiscussionsCount
+				analyzedMergeRequest.UserNotesCount = mergeRequest.UserNotesCount
+				analyzedMergeRequest.CommitCount = len(mergeRequest.Commits.Nodes)
+				analyzedMergeRequest.ReviewTimeInDays = reviewTimeDay
+				analyzedMergeRequest.CommitToMergeRequestionCreationInDays = commitToMergeRequestionCreationTimeDay
+				analyzedMergeRequest.TotalCycleTimeInDays = totalCycleTimeDay
+				analyzedMergeRequest.Url = mergeRequest.Url
+
+				analyzedMergeRequestsByTeam[team][week] = append(analyzedMergeRequestsByTeam[team][week], analyzedMergeRequest)
+			} else {
+				fmt.Println("ERROR team not found for " + mergeRequest.Author.Name)
 			}
 		}
 
-		_, week := timeMergedAt.ISOWeek()
-
-		cycleTime := timeMergedAt.Sub(firstActivityDate).Hours()
-		totalCycleTimeDay := cycleTime / 24
-
-		reviewTime := timeMergedAt.Sub(timeCreatedAt).Hours()
-		reviewTimeDay := reviewTime / 24
-
-		commitToMergeRequestionCreationTime := timeCreatedAt.Sub(firstActivityDate).Hours()
-		commitToMergeRequestionCreationTimeDay := commitToMergeRequestionCreationTime / 24
-
-		team := membersTeam[mergeRequest.Author.Name]
-
-		if _, ok := analyzedMergeRequestsByTeam[team]; ok {
-			var analyzedMergeRequest AnalyzedMergeRequest
-			analyzedMergeRequest.Id = mergeRequest.Id
-			analyzedMergeRequest.Author = mergeRequest.Author.Name
-			analyzedMergeRequest.UserDiscussionsCount = mergeRequest.UserDiscussionsCount
-			analyzedMergeRequest.UserNotesCount = mergeRequest.UserNotesCount
-			analyzedMergeRequest.CommitCount = len(mergeRequest.Commits.Nodes)
-			analyzedMergeRequest.ReviewTimeInDays = reviewTimeDay
-			analyzedMergeRequest.CommitToMergeRequestionCreationInDays = commitToMergeRequestionCreationTimeDay
-			analyzedMergeRequest.TotalCycleTimeInDays = totalCycleTimeDay
-			analyzedMergeRequest.Url = mergeRequest.Url
-
-			analyzedMergeRequestsByTeam[team][week] = append(analyzedMergeRequestsByTeam[team][week], analyzedMergeRequest)
-		} else {
-			fmt.Println("ERROR team not found for " + mergeRequest.Author.Name)
+		for keyTeamName, valueAnalyzedMergeRequestsByWeek := range analyzedMergeRequestsByTeam {
+			fmt.Printf("Team: %s\n", keyTeamName)
+			for keyWeek, valueAnalyzedMergeRequests := range valueAnalyzedMergeRequestsByWeek {
+				fmt.Printf("Week: %d ->\n", keyWeek)
+				for _, analyzedMergeRequest := range valueAnalyzedMergeRequests {
+					fmt.Printf("\t%+v\n", analyzedMergeRequest)
+				}
+			}
 		}
+
+		// defer the closing of our jsonFile so that we can parse it later on
+		defer jsonMergeRequests.Close()
 	}
 
-	for keyTeamName, valueAnalyzedMergeRequestsByWeek := range analyzedMergeRequestsByTeam {
-		fmt.Printf("Team: %s\n", keyTeamName)
-		for keyWeek, valueAnalyzedMergeRequests := range valueAnalyzedMergeRequestsByWeek {
-			fmt.Printf("Week: %d ->\n", keyWeek)
-			for _, analyzedMergeRequest := range valueAnalyzedMergeRequests {
-				fmt.Printf("\t%+v\n", analyzedMergeRequest)
-			}
-		}
-	}
-
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonMergeRequests.Close()
+	defer f.Close()
 }
 
 func ReadTeamsDataJson(analyzedMergeRequestsByTeam map[string]map[int][]AnalyzedMergeRequest) map[string]string {
